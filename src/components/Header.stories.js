b278b18ac2d9ced727b42f41439db2d8c3b2a832
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-10-30 01:13:56 +0900

    Headerコンポーネントのアクション定義

--- src/components/Header.stories.js
+++ src/components/Header.stories.js
@@ -1,13 +1,21 @@
 import Header from './Header'
 
 export default {
-  title: 'Header'
+  title: 'Header',
+  parameters: { actions: { argTypesRegex: '^on.*' } }
 }
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Header },
-  template: `<Header v-bind="$props" /> `
+  template: `
+    <Header
+      @onLogout="onLogout"
+      @onLogin="onLogin"
+      @onSignUp="onSignUp"
+      v-bind="$props"
+    />
+  `
 })
 
 export const LoggedIn = Template.bind({})

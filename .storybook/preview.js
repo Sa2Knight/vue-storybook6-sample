276cd58b555630d40e1ac923ca06956e26f7714e
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-10-31 22:50:14 +0900

    デフォルトの背景色を設定

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,15 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-
 export const parameters = {
-  viewport: {
-    viewports: INITIAL_VIEWPORTS
+  backgrounds: {
+    default: 'twitter',
+    values: [
+      {
+        name: 'twitter',
+        value: '#00aced'
+      },
+      {
+        name: 'facebook',
+        value: '#3b5998'
+      }
+    ]
   }
 }

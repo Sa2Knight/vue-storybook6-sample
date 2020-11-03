acb94c2c1d74bd3e122da2ab058844f74e79ede8
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-11-03 17:40:52 +0900

    余白を設定するツールバーを追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,27 @@
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-
-export const parameters = {
-  viewport: {
-    viewports: INITIAL_VIEWPORTS
+export const globalTypes = {
+  padding: {
+    name: 'Padding',
+    description: 'ストーリー描画時のpaddingを設定します',
+    defaultValue: '0px',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['0px', '30px', '100px']
+    }
   }
 }
+
+export const decorators = [
+  (story, context) => ({
+    props: {
+      padding: {
+        type: String,
+        default: context.globals.padding
+      }
+    },
+    template: `
+      <div :style="{padding: padding}">
+        <story />
+      </div>
+    `
+  })
+]
